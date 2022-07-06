96af7cccb9d9c31dea7f75f982ff12e70a30c1af
Matas Okayer <masatokereke@gmail.com>
2022-07-05 18:35:00 -0700

    Add functionality for special buttons

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ let toScreen = "0";
 let num1_str = "0", num2_str = null;
 let operator = "add";
 
+let hasDec = false;
+
 let operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach(x => x.addEventListener('mousedown', e => {operate(num1_str, num2_str, operator);
                                                                    operator = e.target.classList[0];}));
 
 let numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(x => x.addEventListener('mousedown', e => inputNum(e.target.classList[0])));
+
+let specialButton = document.querySelector(".AC");
+specialButton.addEventListener('mousedown', ()=>reset());
+
+specialButton = document.querySelector(".pm")
+specialButton.addEventListener('mousedown', ()=>{toScreen = num2_str=(+num2_str*(-1)).toString(); updateScreen();});
+
+specialButton = document.querySelector(".E");
+specialButton.addEventListener('mousedown', () => E());
+
+specialButton = document.querySelector(".dec");
+specialButton.addEventListener('mousedown', () => addDecimal()); 
+
+specialButton = document.querySelector(".backspace");
+specialButton.addEventListener('mousedown', () => backspace());
+
+
 
 let screen = document.querySelector(".screen");
 
@@ -38,7 +57,7 @@ function updateScreen() {
 
 function inputNum(num) {
     if (num2_str === null) num2_str = "";
-    if (num2_str.length >= 9) return;
+    if (num2_str.length >= 10 || (!hasDec && num2_str.length >= 9)) return;
     num2_str += num; 
     toScreen = num2_str;
     updateScreen();
@@ -59,14 +78,14 @@ function divide(num1, num2) {
 }
 function equals(num1, num2) {
     if (num2_str === null) return;
-    num1_str = "0";
-    num2_str = null;
     return num2;
 }
 
 
 function operate(num1, num2, operator) {
     if (num2_str === null) return;
+
+    hasDec = false;
 
     num1 = +num1;
     num2 = +num2;
@@ -76,6 +95,25 @@ function operate(num1, num2, operator) {
     
     toScreen = num1_str;
     updateScreen();
+}
+
+function addDecimal() {
+    if (hasDec) return;
+    if (num2_str == null) num2_str = "0";
+    toScreen = num2_str += ".";
+    hasDec = true;
+    updateScreen();
+}
+
+function backspace() {
+    if (num2_str.length <= 0) return;
+    toScreen = num2_str = num2_str.length > 1 ? num2_str.substring(0, num2_str.length - 1) : "0";
+    updateScreen();
+}
+
+function E() {
+    reset();
+    screen.textContent = "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE";
 }
 
 reset();
